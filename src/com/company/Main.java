6b2b353678282a65da3102940df8230acb9c7638
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-30 14:57:08 +0300

    LeapYear udemy

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code
+        System.out.println(isLeapYear(-1600));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+        if ((year <= 1) || (year >= 9999)) {
+            return false;
+        }
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
